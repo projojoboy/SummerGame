bd5c1b75f8bd7aba5d3782f88aab4e57e462260a
samjerry <jerry.sam@ziggo.nl>
2018-07-18 20:50:45 +0200

    SpellScriptableObject
    Created a scriptable object which allows us to easily create new spells

---
+++ SummerGame/Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ability", menuName = "Ability/New")]
+public class Ability : ScriptableObject {
+
+	[Header("\tSpell Information")]
+	[SerializeField]private string _name;
+	[SerializeField, Tooltip("e.g physical, fire, nature, death, water, ice")]private string _type;
+	[SerializeField]private string _desc;
+
+	[SerializeField]private GameObject _spellPrefab;
+
+	[SerializeField]private float _mCost;
+	[SerializeField]private float _spellDmg;
+
+//---------------------------------//
+//---- AREA OF EFFECT SETTINGS ----//
+//---------------------------------//		
+	[Header("\tAOE Settings")]
+	[SerializeField, Tooltip("Area Of Effect")]private bool _isAOE;
+	[SerializeField]private float _aoeDmg;
+
+//-----------------------------------//
+//---- DAMAGE OVER TIME SETTINGS ----//
+//-----------------------------------//	
+	[Header("\tDOT Settings")]
+	[SerializeField, Tooltip("Damage Over Time")]private bool _isDOT;
+	[SerializeField, Range(1,4)]private int _dotDur;
+	[SerializeField]private float _dotDmg;
+
+//------------------------------------//
+//---- HEALING OVER TIME SETTINGS ----//
+//------------------------------------//
+	[Header("\tHOT Settings")]
+	[SerializeField, Tooltip("Healing Over Time")]private bool _isHOT;
+	[SerializeField, Range(1,4)]private int _hotDur;
+	[SerializeField]private float _hotAmount;
+
+
+	public void GetAbility() {
+		_aoeDmg = Mathf.Round(_spellDmg / 3);
+		_dotDmg = Mathf.Round(_spellDmg / 4);
+		_hotAmount = Mathf.Round(_spellDmg / 3);
+
+		Debug.Log("name: " + _name +
+		" type: " + _type +
+		" description: " + _desc +
+		" cost: " + _mCost +
+		" dmg: " + _spellDmg +
+		" AOE: " + _isAOE +
+		" AOEdmg: " + _aoeDmg +
+		" DOT: " + _isDOT +
+		" DOTdur: " + _dotDur +
+		" DOTdmg: " + _dotDmg +
+		" HOT: " + _isHOT + 
+		" HOTdur: " + _hotDur +
+		" HOTamount: " + _hotAmount);
+	}
+}
---
+++ SummerGame/Assets/Scripts/Abilities/AbilityType.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityType : MonoBehaviour {
+
+	[SerializeField]private Ability _ability0;
+	[SerializeField]private Ability _ability1;
+	[SerializeField]private Ability _ability2;
+	[SerializeField]private Ability _ability3;
+
+	private void Start() {
+		_ability0.GetAbility();
+		_ability1.GetAbility();
+		_ability2.GetAbility();
+		_ability3.GetAbility();
+	}
+}
